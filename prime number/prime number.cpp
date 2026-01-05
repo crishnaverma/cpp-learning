3acdadfebda4cf3c68e50e21e41b6b9dbae72978
crishnaverma <crishnaverma@gmail.com>
2026-01-05 21:53:51 +0530

    correction

--- prime number/prime number.cpp
+++ prime number/prime number.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 int main(){
-    int n = 20;
+    int n = 7;
 
-    for (int i = 1; i <= (n-1); i++){
+    for (int i = 2; i <= (n-1); i++){
         if(n%i == 0){
             cout<< n << " is not a prime number\n";
             break;
         }
+
+        else{
+            cout<< n <<" is a prime number\n";
+            break;
+        }
     }
+    return 0;
 }
