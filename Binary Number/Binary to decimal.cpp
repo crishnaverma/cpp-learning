57b7c4879101cfc123f7ebeb191a4c906fdb0b79
crishnaverma <crishnaverma@gmail.com>
2026-01-08 12:25:04 +0530

    Binary number

---
+++ Binary Number/Binary to decimal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Binary_to_Decimal(int n){
+    int sum = 0 , pow = 1 ;
+    while(n>0){
+        int rem = n%10;
+        n/=10;
+        sum+=(rem*pow);
+        pow*=2;
+
+    }
+    return sum;
+}
+
+int main(){
+    cout<<Binary_to_Decimal(1111);
+    return 0;
+}
